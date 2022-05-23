415bb92a598709e22b1a48586eab39121af59e36
Jayden Fam <jzfam93@live.com>
2022-05-23 09:15:26 +0800

    added postgres

--- cmd/payment/main.go
+++ cmd/payment/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.yml`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		logrus.Debug("Service RUN on DEBUG mode")
+	}
+}
 
 func main() {
 	fmt.Println("Hello, World!")
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,40 @@
 package app
 
 import (
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jaydenjz/accounting/internal/usecase"
+	"github.com/jaydenjz/accounting/pkg/postgres"
+	"github.com/spf13/viper"
 )
 
 func Run() {
-	paymentUseCase := usecase.New(repository.)
+	// Repository
 
+	paymentUseCase := usecase.New()
+	pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax))
 	// HTTP Server
-	handler := gin.Default()
-	http.newRouter(handler, paymentUseCase)
-	handler.Run(":5000")
+	handler := gin.New()
+	v1.newRouter(handler, paymentUseCase)
+	server01 := &http.Server{
+		Addr:         ":8080",
+		Handler:      handler,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+}
+
+func ReadConfigFile() {
+	viper.SetConfigFile("config.json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
 }
